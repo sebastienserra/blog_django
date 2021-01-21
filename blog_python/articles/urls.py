71f7152470b72a8ba9e726dc6f6b64617abc53e8
sebastien <sebaserrac@yahoo.com>
2021-01-21 21:50:47 +0100

    Authentication

--- blog_python/articles/urls.py
+++ blog_python/articles/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'articles'
 
-urlpatterns = [
+urlpatterns = [ # order in list is important
     re_path(r'^$',views.article_list, name='list'),
+    re_path(r'^create/$',views.article_create, name='create'),
     re_path(r'^(?P<slug>[\w-]+)/$',views.article_detail, name='detail'),
 ]
